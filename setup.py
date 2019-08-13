3840dc8206107a2439410321a6f1ab865d25adb2
icoxfog417 <icoxfog417@yahoo.co.jp>
2019-08-13 12:15:08 +0900

    fix bug version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="edinet-python",
-    version="0.1.11",
+    version="0.1.12",
     description="EDINET API Client for Python.",
     long_description=readme,
     long_description_content_type="text/markdown",
